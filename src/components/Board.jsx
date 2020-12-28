dc84f8faff07a37084d334486bacb9da3c3b099e
lazzyms <maulik.sompura06@gmail.com>
2020-12-28 22:44:21 +0530

    tic-tac-toe fully functional

---
+++ src/components/Board.jsx
@@ -0,0 +1,26 @@
+import Square from "./Square";
+import { Row, Card } from "react-bootstrap";
+
+function Board(props) {
+  return (
+    <Card bg="secondary" text="light" style={{ width: "max-content" }}>
+      <Card.Body>
+        {props.squares.map((box, i) => (
+          <Row key={i} className="align-content-center justify-content-center">
+            {box.map((val, j) => (
+              <Square
+                value={val}
+                    key={j}
+                    keyi={i}
+                    keyj={j}
+                setSquare={() => props.setSquare(i, j)}
+              />
+            ))}
+          </Row>
+        ))}
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Board;
---
+++ src/components/Square.jsx
@@ -0,0 +1,27 @@
+import { Badge } from "react-bootstrap";
+
+import { FaTimes, FaRegCircle } from "react-icons/fa";
+
+function Square(props) {
+  return (
+    <Badge
+      variant="dark"
+      className="square"
+      onClick={() => props.setSquare(props.keyi, props.keyj)}
+    >
+      <span>
+        {props.value ? (
+          props.value == "X" ? (
+            <FaTimes size="100px" />
+          ) : (
+            <FaRegCircle size="100px" />
+          )
+        ) : (
+          ""
+        )}
+      </span>
+    </Badge>
+  );
+}
+
+export default Square;
